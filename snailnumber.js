ed29604239025a4dacce8facdf978d444d3e9ffb
SnailKing <39387494+snailking@users.noreply.github.com>
2019-04-08 21:44:38 +0200

    Add files via upload

--- snailnumber.js
+++ snailnumber.js
@@ -288,7 +288,7 @@ function updateFieldEther(){
 	f_ether = document.getElementById('fieldEther').value;
 	if(f_ether < a_bid){
 		f_ether = a_bid;
-		f_ether.value = a_bid;
+		document.getElementById('fieldEther').value = a_bid;
 	}
 }
 
